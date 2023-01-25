ef9fe71ce59dff41c456f7a015f27e997da1e576
Lorenzo Ravaglia <lorenzo.ravaglia@hope.edu>
2023-01-25 16:39:54 -0500

    feat: add d% rng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod random;
 use color_eyre::Result;
 
 #[tokio::main]
---
+++ src/random.rs
@@ -0,0 +1,12 @@
+use lazy_static::lazy_static;
+use nanorand::{Rng, WyRand};
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref RNG: Mutex<WyRand> = Mutex::new(WyRand::new());
+}
+
+pub fn percent() -> u8 {
+    let mut rng = RNG.lock().expect("could not lock RNG");
+    rng.generate_range(1..101)
+}
